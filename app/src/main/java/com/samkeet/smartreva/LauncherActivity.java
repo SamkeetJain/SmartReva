510248d7cc7d6f4f7ec7a753feb5667e0290f51c
Samkeet Jain <jain.sankeet2210@gmail.com>
2016-11-11 23:54:47 +0530

    uploaded border.xml 2

--- app/src/main/java/com/samkeet/smartreva/LauncherActivity.java
+++ app/src/main/java/com/samkeet/smartreva/LauncherActivity.java
@@ -6,10 +6,13 @@ import android.widget.Button;
 
 public class LauncherActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+
 
 
 
