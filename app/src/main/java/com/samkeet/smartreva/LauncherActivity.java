616395761d7561b380c3409a7b6536a5ad5a8478
vaibhavkri <vaibhavkrishna.bhosle@gmail.com>
2016-11-11 23:06:07 +0530

    second commit

--- app/src/main/java/com/samkeet/smartreva/LauncherActivity.java
+++ app/src/main/java/com/samkeet/smartreva/LauncherActivity.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 
 public class LauncherActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+        //second commit
     }
 }
