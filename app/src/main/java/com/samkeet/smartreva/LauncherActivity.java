1a9a97ac20c6762e73b1d63b5b000f216283af6b
vaibhavkri <vaibhavkrishna.bhosle@gmail.com>
2016-11-11 23:08:21 +0530

    third commit

--- app/src/main/java/com/samkeet/smartreva/LauncherActivity.java
+++ app/src/main/java/com/samkeet/smartreva/LauncherActivity.java
@@ -11,5 +11,6 @@ public class LauncherActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
         //second commit
+        //third commit
     }
 }
