b2654413386a2fbc672d5975b6a961e883147b20
Samkeet Jain <jain.sankeet2210@gmail.com>
2016-11-12 18:29:54 +0530

    stable commit

--- app/src/main/java/com/samkeet/smartreva/Attendence/TakeAttendence.java
+++ app/src/main/java/com/samkeet/smartreva/Attendence/TakeAttendence.java
@@ -11,5 +11,6 @@ public class TakeAttendence extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_attendence);
+        //content
     }
 }
