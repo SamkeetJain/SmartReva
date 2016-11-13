dfae20c2b5381fe10c718c270ae291eb17871205
Samkeet Jain <jain.sankeet2210@gmail.com>
2016-11-13 18:55:26 +0530

    changed icons position

--- app/src/main/java/com/samkeet/smartreva/ProfileStudentActivity.java
+++ app/src/main/java/com/samkeet/smartreva/ProfileStudentActivity.java
@@ -4,10 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class ProfileStudentActivity extends AppCompatActivity {
 
     public Button EditProfileStudent, LogoutStudent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,7 +19,14 @@ public class ProfileStudentActivity extends AppCompatActivity {
         LogoutStudent = (Button) findViewById(R.id.LogoutStudent_button);
     }
 
-    public void BackButton(View v){
+    public void LogoutStudent(View v) {
+        Constants.UserData.USER_ID=null;
+        Constants.UserData.TOKEN=null;
+        Constants.SharedPreferenceData.clearData();
+        Toast.makeText(getApplicationContext(),"Loged Out",Toast.LENGTH_SHORT).show();
+    }
+
+    public void BackButton(View v) {
         finish();
     }
 }
