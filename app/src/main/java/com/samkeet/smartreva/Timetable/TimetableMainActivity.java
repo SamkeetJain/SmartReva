4d47760f59c075b4d8cdc5363b6677d294d6c18b
vaibhavkri <vaibhavkrishna.bhosle@gmail.com>
2016-11-14 09:52:48 +0530

    upload nots ui complete

--- app/src/main/java/com/samkeet/smartreva/Timetable/TimetableMainActivity.java
+++ app/src/main/java/com/samkeet/smartreva/Timetable/TimetableMainActivity.java
@@ -3,6 +3,7 @@ package com.samkeet.smartreva.Timetable;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.samkeet.smartreva.R;
 
@@ -12,5 +13,9 @@ public class TimetableMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timetable_main);
+    }
+
+    public void BackButton(View v){
+        finish();
     }
 }
