2a8e5d6d611e084db0e8869a081b04c735c645ce
vaibhavkri <vaibhavkrishna.bhosle@gmail.com>
2016-11-16 13:02:09 +0530

    stable presentation

--- app/src/main/java/com/samkeet/smartreva/Notes/UploadNotes.java
+++ app/src/main/java/com/samkeet/smartreva/Notes/UploadNotes.java
@@ -1,5 +1,6 @@
 package com.samkeet.smartreva.Notes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,16 +9,30 @@ import com.samkeet.smartreva.R;
 
 public class UploadNotes extends AppCompatActivity {
 
+    private static final int PICK_FILE_REQUEST = 101;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_notes);
+
     }
     public void BackButton(View v){
         finish();
     }
 
     public void NotesUploadSelectFile(View v){
+        Intent intent = new Intent();
+        intent.setType("*/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select a File"), PICK_FILE_REQUEST);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode==PICK_FILE_REQUEST){
+            Intent intent=new Intent(getApplicationContext(),UploadToServer.class);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/samkeet/smartreva/Results/ResultsMainActivity.java
+++ app/src/main/java/com/samkeet/smartreva/Results/ResultsMainActivity.java
@@ -3,18 +3,58 @@ package com.samkeet.smartreva.Results;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.samkeet.smartreva.R;
 
 public class ResultsMainActivity extends AppCompatActivity {
 
+    public Spinner semesterDrop,componetDrop;
+    final String semesterItems[]=new String[]{"First", "Second", "Third", "Fourth", "Fifth", "Sixth", "Seventh", "Eigth"};
+    final String componetsItems[]=new String[]{"C1","C2","C3"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results_main);
+
+        semesterDrop = (Spinner) findViewById(R.id.spinner1);
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, semesterItems);
+        semesterDrop.setAdapter(adapter1);
+        semesterDrop.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+//                Toast.makeText(getApplicationContext(),semesterItems[position],Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        componetDrop = (Spinner) findViewById(R.id.spinner2);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, componetsItems);
+        componetDrop.setAdapter(adapter2);
+        componetDrop.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+//                Toast.makeText(getApplicationContext(),componetsItems[position],Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 
-    public void BackButton(View v){
+    public void BackButton(View v) {
         finish();
     }
+
+
 }
