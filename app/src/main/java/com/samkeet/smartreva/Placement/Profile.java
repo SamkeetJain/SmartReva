30f9b3b58b71a7bb4e259be99f1310fc12203725
vaibhavkri <vaibhavkrishna.bhosle@gmail.com>
2016-12-12 07:53:00 +0530

    updated placements 2

--- app/src/main/java/com/samkeet/smartreva/Placement/Profile.java
+++ app/src/main/java/com/samkeet/smartreva/Placement/Profile.java
@@ -3,18 +3,127 @@ package com.samkeet.smartreva.Placement;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import com.samkeet.smartreva.R;
 
 public class Profile extends AppCompatActivity {
 
+    public EditText mName, mDob, mGender, mNationality, mPaddress, mCaddress, mPhone, mMobile, mEmail;
+    public EditText mFname, mFmobile, mFoccupation, mFemail;
+    public EditText mMname, mMmobile, mMoccupation, mMemail;
+    public EditText mTs, mTpy, mTb, mTn, mTm;
+    public EditText mTWs, mTWpy, mTWb, mTWn, mTWm;
+    public EditText mDs, mDpy, mDb, mDn, mDm;
+
+    public String name, dob, gender, nationality, paddress, caddress, phone, mobile, email;
+    public String fname, fmobile, foccupation, femail;
+    public String mname, mmobile, moccupation, memail;
+    public String ts, tpy, tb, tn, tm;
+    public String tws, twpy, twb, twn, twm;
+    public String ds, dpy, db, dn, dm;
+
+    public boolean editable;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        setFindViewById();
+
     }
 
-    public void BackButton (View v){
+    public void setFindViewById() {
+        mName = (EditText) findViewById(R.id.full_name);
+        mDob = (EditText) findViewById(R.id.DOB);
+        mGender = (EditText) findViewById(R.id.gender);
+        mNationality = (EditText) findViewById(R.id.Nationality);
+        mPaddress = (EditText) findViewById(R.id.paddress);
+        mCaddress = (EditText) findViewById(R.id.caddress);
+        mPhone = (EditText) findViewById(R.id.phone);
+        mMobile = (EditText) findViewById(R.id.mobile);
+        mEmail = (EditText) findViewById(R.id.email);
+
+        mFname = (EditText) findViewById(R.id.f_full_name);
+        mFoccupation = (EditText) findViewById(R.id.f_occupation);
+        mFmobile = (EditText) findViewById(R.id.f_mobile);
+        mFemail = (EditText) findViewById(R.id.f_email);
+
+        mMname = (EditText) findViewById(R.id.m_full_name);
+        mMoccupation = (EditText) findViewById(R.id.m_occupation);
+        mMmobile = (EditText) findViewById(R.id.m_mobile);
+        mMemail = (EditText) findViewById(R.id.m_email);
+
+        mMname = (EditText) findViewById(R.id.m_full_name);
+        mMoccupation = (EditText) findViewById(R.id.m_occupation);
+        mMmobile = (EditText) findViewById(R.id.m_mobile);
+        mMemail = (EditText) findViewById(R.id.m_email);
+
+        mTs = (EditText) findViewById(R.id.t_s);
+        mTpy = (EditText) findViewById(R.id.t_py);
+        mTb = (EditText) findViewById(R.id.t_b);
+        mTn = (EditText) findViewById(R.id.t_n);
+        mTm = (EditText) findViewById(R.id.t_m);
+
+        mTWs = (EditText) findViewById(R.id.tw_s);
+        mTWpy = (EditText) findViewById(R.id.tw_py);
+        mTWb = (EditText) findViewById(R.id.tw_b);
+        mTWn = (EditText) findViewById(R.id.tw_n);
+        mTWm = (EditText) findViewById(R.id.tw_m);
+
+        mDs = (EditText) findViewById(R.id.d_s);
+        mDpy = (EditText) findViewById(R.id.d_py);
+        mDb = (EditText) findViewById(R.id.d_b);
+        mDn = (EditText) findViewById(R.id.d_n);
+        mDm = (EditText) findViewById(R.id.d_m);
+
+    }
+
+    public void setEnable(boolean enabled) {
+
+        editable=enabled;
+
+        mName.setEnabled(enabled);
+        mDob.setEnabled(enabled);
+        mGender.setEnabled(enabled);
+        mNationality.setEnabled(enabled);
+        mPaddress.setEnabled(enabled);
+        mCaddress.setEnabled(enabled);
+        mPhone.setEnabled(enabled);
+        mMobile.setEnabled(enabled);
+        mEmail.setEnabled(enabled);
+
+        mFname.setEnabled(enabled);
+        mFoccupation.setEnabled(enabled);
+        mFmobile.setEnabled(enabled);
+        mFemail.setEnabled(enabled);
+
+        mMname.setEnabled(enabled);
+        mMoccupation.setEnabled(enabled);
+        mMmobile.setEnabled(enabled);
+        mMemail.setEnabled(enabled);
+
+        mTs.setEnabled(enabled);
+        mTpy.setEnabled(enabled);
+        mTb.setEnabled(enabled);
+        mTn.setEnabled(enabled);
+        mTm.setEnabled(enabled);
+
+        mTWs.setEnabled(enabled);
+        mTWpy.setEnabled(enabled);
+        mTWb.setEnabled(enabled);
+        mTWn.setEnabled(enabled);
+        mTWm.setEnabled(enabled);
+
+        mDs.setEnabled(enabled);
+        mDpy.setEnabled(enabled);
+        mDb.setEnabled(enabled);
+        mDn.setEnabled(enabled);
+        mDm.setEnabled(enabled);
+    }
+
+    public void BackButton(View v) {
         finish();
     }
 }
