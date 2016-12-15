369fbc8d6267eed269e0982713fbdcdb6edc6d81
Samkeet Jain <jain.sankeet2210@gmai.com>
2016-12-15 23:54:58 +0530

    refined trim and length

--- app/src/main/java/com/samkeet/smartreva/Setings/SettingsMainActivity.java
+++ app/src/main/java/com/samkeet/smartreva/Setings/SettingsMainActivity.java
@@ -13,6 +13,7 @@ public class SettingsMainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_main);
     }
+
     public void BackButton(View v){
         finish();
     }
