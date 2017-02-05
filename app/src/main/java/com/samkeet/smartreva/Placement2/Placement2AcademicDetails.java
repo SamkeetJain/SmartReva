7dba060933dc2641f2b63e62bd3e2cba3d7598f0
leelash <lilashsah2012@gmail.com>
2017-02-05 18:16:09 +0530

    academmin

---
+++ app/src/main/java/com/samkeet/smartreva/Placement2/Placement2AcademicDetails.java
@@ -0,0 +1,15 @@
+package com.samkeet.smartreva.Placement2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.samkeet.smartreva.R;
+
+public class Placement2AcademicDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_placement2_academic_details);
+    }
+}
